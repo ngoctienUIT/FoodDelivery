86a8b0e1da6c44d4ae997056615ba303aecc1122
Trần Ngọc Tiến <94039133+ngoctienUIT@users.noreply.github.com>
2023-09-03 15:32:42 +0700

    update order item

--- app/src/main/java/com/tnt/food_delivery/presentation/order/OrderScreen.kt
+++ app/src/main/java/com/tnt/food_delivery/presentation/order/OrderScreen.kt
@@ -1,8 +1,16 @@
 package com.tnt.food_delivery.presentation.order
 
+import android.annotation.SuppressLint
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,6 +18,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -22,18 +31,23 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -42,9 +56,15 @@ import com.tnt.food_delivery.R
 import com.tnt.food_delivery.ui.components.CustomScaffold
 import com.tnt.food_delivery.ui.components.shadow
 import com.tnt.food_delivery.ui.theme.FoodDeliveryTheme
+import kotlin.math.roundToInt
+
+const val ANIMATION_DURATION = 500
+const val MIN_DRAG_AMOUNT = 6
 
 @Composable
 fun OrderScreen(navController: NavController) {
+    val list = remember { mutableStateListOf(false, false, false, false, false, false) }
+
     CustomScaffold(
         navController = navController,
         title = "Order details",
@@ -52,12 +72,18 @@ fun OrderScreen(navController: NavController) {
     ) {
         LazyColumn(
             modifier = Modifier
-                .padding(horizontal = 25.dp, vertical = 10.dp)
+                .padding(horizontal = 25.dp)
                 .fillMaxSize()
         ) {
-            items(6) {
+            items(6) { index ->
                 Column {
-                    ItemOrder()
+                    ItemOrder(
+                        isRevealed = list[index],
+                        cardOffset = -180f,
+                        onExpand = { list[index] = true },
+                        onCollapse = { list[index] = false },
+                        onRemove = { list[index] = false },
+                    )
                     Spacer(modifier = Modifier.height(20.dp))
                 }
             }
@@ -137,84 +163,140 @@ fun OrderScreen(navController: NavController) {
     }
 }
 
+@SuppressLint("UnusedTransitionTargetStateParameter")
 @Composable
-fun ItemOrder() {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(105.dp)
-            .shadow(
-                color = Color(0xFF5A6CEA).copy(alpha = 0.07f),
-                spread = 25.dp,
-                blurRadius = 50.dp,
-                offsetX = 12.dp,
-                offsetY = 26.dp,
-            ),
-        shape = RoundedCornerShape(22),
-        colors = CardDefaults.cardColors(containerColor = Color.White)
-    ) {
-        Row(
-            modifier = Modifier.fillMaxSize(),
-            verticalAlignment = Alignment.CenterVertically
+fun ItemOrder(
+    isRevealed: Boolean,
+    cardOffset: Float,
+    onExpand: () -> Unit,
+    onCollapse: () -> Unit,
+    onRemove: () -> Unit,
+) {
+    val transitionState = remember {
+        MutableTransitionState(isRevealed).apply { targetState = !isRevealed }
+    }
+    val transition = updateTransition(transitionState, "cardTransition")
+    val offsetTransition by transition.animateFloat(
+        label = "cardOffsetTransition",
+        transitionSpec = { tween(durationMillis = ANIMATION_DURATION) },
+        targetValueByState = { if (isRevealed) cardOffset else 0f },
+    )
+    val cardElevation by transition.animateDp(
+        label = "cardElevation",
+        transitionSpec = { tween(durationMillis = ANIMATION_DURATION) },
+        targetValueByState = { if (isRevealed) 10.dp else 0.dp }
+    )
+
+    Box {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(105.dp),
+            shape = RoundedCornerShape(22.dp),
+            colors = CardDefaults.cardColors(containerColor = Color(0xFFFEAD1D)),
+            elevation = CardDefaults.cardElevation(0.dp)
         ) {
-            Spacer(modifier = Modifier.width(10.dp))
-            Image(
-                modifier = Modifier
-                    .height(65.dp)
-                    .width(65.dp)
-                    .clip(RoundedCornerShape(20)),
-                painter = painterResource(id = R.drawable.avatar),
-                contentDescription = "tnt",
-                contentScale = ContentScale.FillBounds,
-            )
-            Spacer(modifier = Modifier.width(20.dp))
-            Column(modifier = Modifier.weight(1f)) {
-                Text(
-                    text = "Spacy fresh crab",
-                    fontSize = 15.sp,
-                    fontWeight = FontWeight(600),
-                    color = Color(0xFF09051C)
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.End
+            ) {
+                Icon(
+                    modifier = Modifier.clickable { onRemove() },
+                    painter = painterResource(id = R.drawable.icon_trash),
+                    contentDescription = "",
+                    tint = Color.White,
                 )
-                Text(
-                    text = "Waroenk kita",
-                    fontSize = 14.sp,
-                    color = Color(0xFF3B3B3B).copy(0.3f),
-                    letterSpacing = 0.5.sp
+                Spacer(modifier = Modifier.width(25.dp))
+            }
+        }
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(105.dp)
+                .shadow(
+                    color = Color(0xFF5A6CEA).copy(alpha = 0.07f),
+                    spread = 25.dp,
+                    blurRadius = 50.dp,
+                    offsetX = 12.dp,
+                    offsetY = 26.dp,
                 )
-                Text(
-                    text = "$ 35",
-                    fontSize = 19.sp,
-                    fontWeight = FontWeight.Bold,
-                    style = TextStyle(
-                        brush = Brush.linearGradient(
-                            colors = listOf(
-                                Color(0xFF53E88B),
-                                Color(0xFF15BE77)
+                .offset { IntOffset(offsetTransition.roundToInt(), 0) }
+                .pointerInput(Unit) {
+                    detectHorizontalDragGestures { _, dragAmount ->
+                        when {
+                            dragAmount <= MIN_DRAG_AMOUNT -> onExpand()
+                            dragAmount >= -MIN_DRAG_AMOUNT -> onCollapse()
+                        }
+                    }
+                },
+            shape = RoundedCornerShape(22.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.White),
+            elevation = CardDefaults.cardElevation(cardElevation)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Spacer(modifier = Modifier.width(10.dp))
+                Image(
+                    modifier = Modifier
+                        .height(65.dp)
+                        .width(65.dp)
+                        .clip(RoundedCornerShape(20)),
+                    painter = painterResource(id = R.drawable.avatar),
+                    contentDescription = "tnt",
+                    contentScale = ContentScale.FillBounds,
+                )
+                Spacer(modifier = Modifier.width(20.dp))
+                Column(modifier = Modifier.weight(1f)) {
+                    Text(
+                        text = "Spacy fresh crab",
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight(600),
+                        color = Color(0xFF09051C)
+                    )
+                    Text(
+                        text = "Waroenk kita",
+                        fontSize = 14.sp,
+                        color = Color(0xFF3B3B3B).copy(0.3f),
+                        letterSpacing = 0.5.sp
+                    )
+                    Text(
+                        text = "$ 35",
+                        fontSize = 19.sp,
+                        fontWeight = FontWeight.Bold,
+                        style = TextStyle(
+                            brush = Brush.linearGradient(
+                                colors = listOf(
+                                    Color(0xFF53E88B),
+                                    Color(0xFF15BE77)
+                                )
                             )
                         )
                     )
+                }
+                ItemQuantity(
+                    id = R.drawable.icon_remove,
+                    alpha = 0.3f,
+                    onClick = {}
                 )
+                Spacer(modifier = Modifier.width(10.dp))
+                Text(
+                    text = "1",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight(500),
+                    color = Color(0xFF181818),
+                    letterSpacing = 0.57.sp
+                )
+                Spacer(modifier = Modifier.width(10.dp))
+                ItemQuantity(
+                    id = R.drawable.icon_add,
+                    alpha = 1f,
+                    onClick = {}
+                )
+                Spacer(modifier = Modifier.width(10.dp))
             }
-            ItemQuantity(
-                id = R.drawable.icon_remove,
-                alpha = 0.3f,
-                onClick = {}
-            )
-            Spacer(modifier = Modifier.width(10.dp))
-            Text(
-                text = "1",
-                fontSize = 16.sp,
-                fontWeight = FontWeight(500),
-                color = Color(0xFF181818),
-                letterSpacing = 0.57.sp
-            )
-            Spacer(modifier = Modifier.width(10.dp))
-            ItemQuantity(
-                id = R.drawable.icon_add,
-                alpha = 1f,
-                onClick = {}
-            )
-            Spacer(modifier = Modifier.width(10.dp))
         }
     }
 }
